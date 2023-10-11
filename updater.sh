c68c0bf7ed992679e3a4ba5f75a0513147888bab
Samuel Lockton <lockton.sam@gmail.com>
2023-10-11 05:16:01 +0000

    v1.1.63

--- updater.sh
+++ updater.sh
@@ -5,7 +5,7 @@ subsubversion=$(cat /home/lockton_sam/.subsubversion);
 cd /var/www/html;
 
 git add *
-git commit -m "auto backup v1.1.$subsubversion"
+git commit -m "v1.1.$subsubversion"
 
 if ! [ "$(git push 2>&1)" = "Everything up-to-date" ]; then
     ((subsubversion++))
