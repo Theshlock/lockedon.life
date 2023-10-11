33726615c853be5e4d94a7df662366393bd709a3
Samuel Lockton <lockton.sam@gmail.com>
2023-10-11 05:39:01 +0000

    v1.1.72

--- mandel-workers.js
+++ mandel-workers.js
@@ -884,7 +884,7 @@ function gameloop() {
 		console.log("x:", xnorm-portalX);
 		console.log("y:", ynorm-portalY);
 //		zoom *= 1 + 0.01*multiplier;
-		zoom *= xnorm-portalX;
+		zoom *= Math.abs(xnorm-portalX);
 
 		time = Date.now();
 		screenX = Math.round(-xnorm * zoom + canvasWidth/2);
