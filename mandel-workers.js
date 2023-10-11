cf6810427806932ee1ce423fa4cbf7e0c82709a7
Samuel Lockton <lockton.sam@gmail.com>
2023-10-11 06:01:01 +0000

    v1.1.78

--- mandel-workers.js
+++ mandel-workers.js
@@ -882,6 +882,7 @@ function gameloop() {
 
 		//compute distance of position to portal
 		console.log("x:", (xnorm-portalX) * zoom);
+		multiplier=Math.pow(2,2 + -Math.log10((xnorm-portalX)* zoom));
 		zoom *= 1 + 0.01*multiplier;
 //		zoom = zoom / Math.abs(xnorm-portalX);
 
