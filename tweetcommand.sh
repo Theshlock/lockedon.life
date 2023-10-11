af2ff70fe00899cc00cd919b839169861fb0cccb
Samuel Lockton <lockton.sam@gmail.com>
2023-10-11 03:29:01 +0000

    auto backup v1.1.37

--- tweetcommand.sh
+++ tweetcommand.sh
@@ -19,7 +19,7 @@ PARAM=$(urlencode "oauth_consumer_key=$consumer_key&oauth_nonce=$nonce&oauth_sig
 signature=$(echo -n 'POST&'$encodedURL'&'$PARAM|openssl dgst -sha1 -binary -hmac "$consumer_secret&$token_secret" |base64)
 encoded_signature=$(urlencode $signature)
 
-data="dynamic message"
+data="dynamic message2"
 echo $data
 curl -v  -X POST \
   $url \
