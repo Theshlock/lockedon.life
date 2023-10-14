2bc0ff081803090dc6062583e5fe3cb22cb71b7c
Samuel Lockton <lockton.sam@gmail.com>
2023-10-14 23:20:01 +0000

    v1.1.152

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-subsubversion=$(cat /home/lockton_sam/.subsubversion);
+subsubversion=$(cat /var/www/html/.subsubversion);
 
 cd /var/www/html;
 
@@ -9,6 +9,6 @@ git commit -m "v1.1.$subsubversion"
 
 if ! [ "$(git push 2>&1)" = "Everything up-to-date" ]; then
     ((subsubversion++))
-    echo $subsubversion > /home/lockton_sam/.subsubversion
+    echo $subsubversion > /var/www/html/.subsubversion
     ./tweet.sh $subsubversion
 fi
